f0b95434f679d0fc03db28c1c7309fd996ef40a2
LeThiKimMy <63316690+ltkmy19@users.noreply.github.com>
2021-10-25 04:01:04 +0700

    add hover.css + fade effect

--- src/cs300/src/Home.js
+++ src/cs300/src/Home.js
@@ -1,31 +1,21 @@
 import React from 'react'
 import {BiSearchAlt} from "react-icons/bi"
 import Background from "./img/book-wallpaper-22142-22699-hd-wallpapers.jpg";
-import "./Page.css"
+import "./Page.css";
+
 function Home() {
+
     return (
+      
 <body  > 
   <div  style={{backgroundImage: `url(${Background})`,  opacity: `0.8`}}>
-    <div class="input-group bg-img"  style={{paddingTop:`50%`,paddingLeft:`35%`, paddingRight:`35%`,}}>
+    <div class="input-group bg-img fade"  style={{paddingTop:`10%`,paddingLeft:`35%`, paddingRight:`35%`,}}>
   <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search"
   aria-describedby="search-addon" />
   <a type="button" class="btn btn-outline-primary" href="/Aboutus">search</a> </div>
   </div>
-  <h1 class="headline">
-	  Widget Inc.
-
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    hihihihihihihdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-    </h1>
 
   </body>
-
-
     )
 }
 
